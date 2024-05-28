6f25abc74271d15114b966033af116422337569f
Saftarli <tofiqsefterli@gmail.com>
2024-05-28 08:56:52 +0400

    feat: created static file

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import index, contact, about, blog
+from .views import index, contact, about, blog, doctors, services, department, pricing, gallery
 
 urlpatterns = [
     path('', index, name='index'),
     path('contact', contact, name='contact'),
     path('about', about, name='about'),
-    path('blog', blog, name='blog')
+    path('blog', blog, name='blog'),
+    path('doctors', doctors, name='doctors'),
+    path('services', services, name='services'),
+    path('department', department, name='department'),
+    path('pricing', pricing, name='pricing'),
+    path('gallery', gallery, name='gallery'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -16,3 +16,25 @@ def about(request):
 
 def blog(request):
     return render(request, 'blog.html')
+
+
+def doctors(request):
+    return render(request, 'doctors.html')
+
+
+def services(request):
+    return render(request, 'services.html')
+
+
+def department(request):
+    return render(request, 'department.html')
+
+
+def pricing(request):
+    return render(request, 'pricing.html')
+
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+
